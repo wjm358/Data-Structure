0df064a6d3fbac8481aae94bb3c9535b939b5a9c
Won JJong <wjm358@nate.com>
2017-06-18 00:57:44 +0900

    BinarySearchTree implement

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,166 +1,250 @@
-package jongmin;
+package data_structure;
 
-public class BinarySearchTree {
- private Node root;// 루트 노드 인스턴스 생성
+public class BinarySearchTree<T extends Comparable<T>> {
+	public Node root;
 
- // 이진 탐색 트리 생성자
- public BinarySearchTree() {
-  root = null;
- }
+	// 초기화 생성자
+	public BinarySearchTree() {
+		root = null;
+	}
 
- public boolean isEmpty() {
-  return root == null;
- }
+	// data를 가진 노드 반환
+	public Node searchNode(T data) {
+		Node temp = root;
 
- public void clear() {
-  root = null;
- }
+		int compare = data.compareTo(temp.getData());
+		while (temp != null) {
+			if (compare < 0)
+				temp.getLeft();
+			else if (compare > 0)
+				temp.getRight();
+			else
+				return temp;
+		}
+		return null;
 
- public void inOrder() {
-  root.inOrder();
- }
+	}
 
- public void preOrder() {
-  root.preOrder();
- }
+	// 노드 삽입
+	public void insertNode(T data) {
 
- public void postOrder() {
-  root.postOrder();
- }
+		// 루트가 공백일 때
+		if (root == null) {
+			root = new Node(data);
+			return;
+		}
 
- /**
-  * 주어진 인터져 t 로 새로운 노드를 생성, 삽입한다
-  * 
-  * @param 삽입할 인터져 t
-  * @return 삽입이 성공하면 true 갑을, 실패하면 false 값을 리턴
-  */
- public boolean insert(int t) {
-  Node newNode = new Node(t);// 주어진 인터져 t 로 새 노드를 생성
-  Node pointer;// 노드를 삽입할 위치까지 트리를 순회해야하니, 포인터 인스턴스를 생성한다
-  boolean insertComplete = false;
-  if (root == null) {
-   // 트리가 empty 상태일 경우, 루트 노드에 삽입
-   root = newNode;
-  } else {
-   pointer = root;
-   // while 루프로 순회를 시작한다.
-   while (!insertComplete) {
-    if (pointer.getData() > t) {
-     // 인터져 t 를 각 노드들과 비교하여, 작으면 
-     // 왼쪽 자식으로, 크면 오른쪽 자식으로 이동한다
-     if (pointer.getLeft() != null) {
-      pointer = pointer.getLeft();
-     } else {
-      pointer.setLeft(newNode);
-      insertComplete = true;
-      break;
-      // 노드 삽입이 완료됬다면, 더이상의 루핑은 불필요하다.
-      // break 구문을 이용, 루프를 종료하자
-     }
-    } else if (pointer.getData() < t) {
-     if (pointer.getRight() != null) {
-      pointer = pointer.getRight();
-     } else {
-      pointer.setRight(newNode);
-      insertComplete = true;
-      break;
-     }
-    } else {
-     // 만약 트리에 이미 중복되는 노드가 존재한다면
-     // 삽입은 불필요하다. 그러니 루핑을 종료한다
-     break;
-    }
-   }
-  }
-  // 삽입이 성공적이라면 true 값을
-  // 중복 노드가 존재한다거나, 예상외 상황이 있어서 삽입을 실패했다면 false 값을 반환한다.
-  return insertComplete;
- }
+		// 데이터가 루트보다 크면 오른쪽으로 이동, 작으면 왼쪽으로 이동
+		Node tempRoot = root;
+		Node parentNode = null;
+		while (tempRoot != null) {
+			parentNode = tempRoot;
+			int compare = data.compareTo(tempRoot.getData());
+			if (compare < 0)
+				tempRoot = tempRoot.getLeft();
+			else if (compare > 0)
+				tempRoot = tempRoot.getRight();
+			else
+				return;
+		}
 
- /**
-  * 인터져 t 를 가진 노드를 트리에서 제거한다
-  * 
-  * @param t 제거할 값 인터져 t
-  * @return 성공적으로 제거했을 때 true 값을, 실패했을 때 false 값을 리턴한다
-  */
- public boolean remove(int t) {
-  Node pointer, parentPointer;
-  pointer = parentPointer = root;
-  while (pointer != null && pointer.getData() != t) {
-   // 삭제할 노드를 찾을 때까지 루핑으로 트리를 순회한다
-   parentPointer = pointer;// parentPointer 인스턴스가 삭제할 노드의 부모 노드를 참조하도록 한다.
-   if (pointer.getData() > t) {
-    pointer = pointer.getLeft();
-   } else {
-    pointer = pointer.getRight();
-   }
-  }
+		// 데이터가 루트보다 크면 오른쪽에 삽입, 작으면 왼쪽에 삽입
+		Node newNode = new Node(data);
+		if (data.compareTo(parentNode.getData()) < 0)
+			parentNode.setLeft(newNode);
+		else
+			parentNode.setRight(newNode);
+	}
 
-  if (pointer == null) {
-   // 첫번째 상황: 트리에 삭제할 노드가 존재하지 않을때
-   return false;
-  } else {
-   if (pointer == root && pointer.getLeft() == null) {
-    // 두번째 상황: 제거할 노드가 루트 노드이고, 루트노드가 왼쪽 자식 노드를 가지지 않을 때
-    root = root.getRight();
-   } else if (pointer != root && pointer.getLeft() == null) {
-    // 세번째 상황: 루트 노드가 아닌 제거할 노드가 왼쪽 자식 노드를 가지지 않을 때
-    if (pointer == parentPointer.getLeft()) {
-     // 제거할 노드가 부모 노드의 왼쪽에 위치할 때
-     parentPointer.setLeft(pointer.getRight());
-    } else {
-     // 제거할 노드가 부모 노드의 오른쪽에 위치할 때
-     parentPointer.setRight(pointer.getRight());
-    }
-   } else {
-    // 네번째 상황: 제거할 노드가 2개의 자식 노드 모두를 가지고 있을 때
-    // 이러한 상황에서 우리는 제거할 노드에서 파생된 서브 트리에서 가장 큰 노드를 검색한 후, 
-    // 제거할 노드의 위치에 삽입할 것 이다.
-    // BST에서 가장 오른쪽에 위치한 노드는 가장 큰 값을 가졌다는걸 기억하자.
-    // 그러니 BST의 왼쪽 서브 트리의 가장 오른쪽에 존재하는 잎(Leaf)노드는 루트 노드의 가장 좋은 대체제 이다.
-    Node rightMostNode = pointer;
-    // while 루핑을 이용해서 가장 오른쪽 노드로 이동하자
-    while (rightMostNode.getRight() != null) {
-     rightMostNode = rightMostNode.getRight();
-    }
-    // 제거할 노드의 데이터 값을 가장 오른쪽 노드의 데이터 값으로 바꿔주자
-    pointer.setData(rightMostNode.getData());
-    rightMostNode = null;// 오른쪽 노드를 null로 만든다
-   }
-  }
-  return true;
- }
+	//노드 삭제
+	public void deleteNode(T data) {
+		boolean isLeft = false;
+		Node current = root;
+		Node parent = root;
 
- /**
-  * 인터져 t 값을 가진 노드를 검색한다
-  * 
-  * @param t 검색할 인터져 t
-  * @return 존재한다면 true 값을, 그렇지 않다면 false 값을 반환한다.
-  */
- public boolean serach(int t) {
-  Node pointer;
-  if (root.getData() == t) {
-   // 루트 노드가 값을 가지고 있는지 확인
-   return true;
-  } else {
-   // 값을 찾을 때 까지 트리를 순회하자
-   pointer = root;
-   while (pointer != null) {
-    if (pointer.getData() > t) {
-     pointer = pointer.getLeft();
-    } else if (pointer.getData() < t) {
-     pointer = pointer.getRight();
-    } else {
-     // 인터져 t가 노드와 비교 했을 때 크지도 작지도 않다면, 이는 값이 일치함을 말한다. 
-     // 루핑을 종료한다
-     break;
-    }
-   }
-  }
-  // 만약 트리에 인터져 t 값을 가진 노드가 존재하지 않는다면
-  // 위의 while 루프에서 트리의 끝까지 여행하게 될거고, 결국 pointer 인스턴스는 null 값을 참조하게 될 것 이다
-  // 하지만 트리에 t값을 가진 노드가 존재한다면, pointer 인스턴스는 그 노드를 참조할 것 이다.
-  // 그렇니 pointer 인스턴스가 null 값을 가지지 않는다면, 트리에 t 값을 가진 노드가 존재한다고 볼 수 있다.
-  return pointer != null;
- }
+		//current가 null이면 root가 null이므로 종료
+		if (current == null)
+			return;
+		
+		//루트가 null이 아니거나 데이터가 탐색되지 않을 때 까지
+		while ( data.compareTo(current.getData()) != 0) {
+			//parent는 current의 전 노드
+			parent = current;
+			int compare = data.compareTo(current.getData());
+			
+			if (compare < 0) {
+				//작으면 왼쪽으로 이동
+				current = current.getLeft();
+				isLeft = true;
+			} else {
+				//크면 오른쪽으로 이동
+				current = current.getRight();
+				isLeft = false;
+			}
+		}
+		
+	
+		//지우려는 노드(current) 의 왼쪽과 오른쪽 노드가 둘다 없을 때 (즉 단말 노드일 때)
+		if (current.getLeft() == null && current.getRight() == null) {
+			//current가 root일때
+			if (current == root)
+				root = null;
+			//parent의 왼쪽에 current가 있을 때 parent의 왼쪽을 null로 만듬
+			else if (parent.getLeft() == current)
+				parent.setLeft(null);
+			 //parent의 오른쪽에 current가 있을 때 parent의 오른쪽을 null로 만듬
+			else
+				parent.setRight(null);
+		} 
+		//지우려는 노드(current)가 오른쪽 노드만 있을 때
+		else if (current.getLeft() == null) {
+			//parent의 오른쪽 노드를 current의 오른쪽 노드로 설정
+			parent.setRight(current.getRight());
+		} 
+		//지우려는 노드(current)가 왼쪽 노드만 있을 때
+		else if (current.getRight() == null) {
+			//parent의 왼쪽 노드를 current의 왼쪽 노드로 설정
+			parent.setLeft(current.getLeft());
+		} 
+		/*지우려는 노드(current)가 왼쪽 노드와 오른쪽 노드 둘다 가지고 있을 때
+		 * current의 오른쪽 노드의 가장 왼쪽노드를 current 자리로 옮겨야한다.
+		 */
+		else {
+
+			Node succParent = current;
+			Node succ = current.getRight();
+			
+			while (succ.getLeft() != null) {
+				succParent = succ;
+				succ = succ.getLeft();
+			}
+
+			
+			if (current != succParent) {
+				succParent.setLeft(succ.getRight());
+				succ.setRight(current.getRight());
+			}
+			
+			//current가 root면 
+			if (current == root)
+				root = succ;
+			
+			//current가 parent의 왼쪽 노드일 때
+			else if (isLeft)
+				parent.setLeft(succ);
+			//current가 parent의 오른쪽 노드일 때
+			else
+				parent.setRight(succ);
+			
+			//current의 왼쪽 노드를 succ의 왼쪽 노드로 설정
+			succ.setLeft(current.getLeft()); 
+		}
+
+	}
+
+	   public Node getSuccessor(Node deleleNode){
+	        Node successsor =null;
+	        Node successsorParent =null;
+	        Node current = deleleNode.getRight();
+	        while(current!=null){
+	            successsorParent = successsor;
+	            successsor = current;
+	            current = current.getLeft();
+	        }
+	        if(successsor!=deleleNode.getRight()){
+	            successsorParent.setLeft(successsor.getRight());
+	            successsor.setRight(deleleNode.getRight());
+	        }
+	        return successsor;
+	    }
+
+	// 노드 구현
+	public class Node {
+		T data;
+		Node left;
+		Node right;
+
+		Node(T data) {
+			this.data = data;
+			left = right = null;
+		}
+
+		// 전위 순회
+		public void preOrder(Node root) {
+			if (root == null)
+				return;
+			else {
+				root.preOrder(root.getLeft());
+				System.out.println(root.getData());
+				root.preOrder(root.getRight());
+			}
+		}
+
+		// 중위 순회
+		public void inOrder(Node root) {
+			if (root == null)
+				return;
+			else {
+				System.out.println(root.getData());
+				root.inOrder(root.getLeft());
+				root.inOrder(root.getRight());
+			}
+		}
+
+		// 후위 순회
+		public void postOrder(Node root) {
+			if (root == null)
+				return;
+			else {
+				root.postOrder(root.getLeft());
+				root.postOrder(root.getRight());
+				System.out.println(root.getData());
+			}
+		}
+
+		// 레벨 순회
+		public void levelOrder(Node root) {
+			QueueFromList<Node> queue = new QueueFromList<Node>();
+
+			if (root == null)
+				return;
+			else {
+				queue.enQueue(root);
+				while (!queue.isEmpty()) {
+					Node temp = queue.deQueue();
+					System.out.print(temp.getData() + " --> ");
+					if (temp.getLeft() != null)
+						queue.enQueue(temp.getLeft());
+					if (temp.getRight() != null)
+						queue.enQueue(temp.getRight());
+				}
+				System.out.println();
+			}
+		}
+
+		public T getData() {
+			return data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+
+		public Node getLeft() {
+			return left;
+		}
+
+		public void setLeft(Node left) {
+			this.left = left;
+		}
+
+		public Node getRight() {
+			return right;
+		}
+
+		public void setRight(Node right) {
+			this.right = right;
+		}
+
+	}
 }
